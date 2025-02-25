109edf5c5721ce8993f0f7ed66ee65ba8459d6f7
fcendesu <furkan.ckmk@outlook.com>
2025-02-25 13:44:12 +0200

    Add NativeWind integration and create initial screens for navigation

--- app/(roots)/(tabs)/index.tsx
+++ app/(roots)/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -9,7 +10,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Sign In</Link>
+      <Link href="/profile">Sign In</Link>
+      <Link href="/properties/1">Sign In</Link>
     </View>
   );
 }
