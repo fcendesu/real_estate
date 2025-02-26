df27551413dfb508632b73a9d15e8e4bf9cb46fe
fcendesu <furkan.ckmk@outlook.com>
2025-02-26 22:18:27 +0200

    Add global state management with GlobalProvider and integrate Appwrite for authentication

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,38 +1,66 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import images from "@/constants/images";
-import icons from "@/constants/icons";
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-const SignIn = () => {
-  const handleLogin = () => {
-    console.log("Login");
+import { login } from "@/lib/appwrite";
+import { Redirect } from "expo-router";
+import { useGlobalContext } from "@/lib/global-provider";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+
+const Auth = () => {
+  const { refetch, loading, isLogged } = useGlobalContext();
+
+  if (!loading && isLogged) return <Redirect href="/" />;
+
+  const handleLogin = async () => {
+    const result = await login();
+    if (result) {
+      refetch();
+    } else {
+      Alert.alert("Error", "Failed to login");
+    }
   };
 
   return (
     <SafeAreaView className="bg-white h-full">
-      <ScrollView contentContainerClassName="h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
         <Image
           source={images.onboarding}
           className="w-full h-4/6"
           resizeMode="contain"
         />
+
         <View className="px-10">
           <Text className="text-base text-center uppercase font-rubik text-black-200">
-            Welcome to ReState
+            Welcome To Real Scout
           </Text>
-          <Text className="text-3xl font-rubik-bold text-black-300 text-center m-2">
-            Let's Get You Closer to {"\n"}
+
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer To {"\n"}
             <Text className="text-primary-300">Your Ideal Home</Text>
           </Text>
-          <Text className="text-center font-rubik text-lg text-black-200  mt-12">
-            Login to ReState with Google
+
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to Real Scout with Google
           </Text>
+
           <TouchableOpacity
             onPress={handleLogin}
             className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
           >
-            <View className="flex-row justify-center items-center">
+            <View className="flex flex-row items-center justify-center">
               <Image
                 source={icons.google}
                 className="w-5 h-5"
@@ -49,4 +77,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default Auth;
